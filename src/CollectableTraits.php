98a5a3b9ab4867d23e1f77cc7519bab84a388732
Peter Gorgone <peter@peltronic.com>
2018-06-14 18:26:20 -0700

    Don't re-use collector object: create new each time so filters don't 'stick'

--- src/CollectableTraits.php
+++ src/CollectableTraits.php
@@ -3,21 +3,18 @@ namespace PsgcLaravelPackages\Collector;
 
 trait CollectableTraits
 {
-    protected static $_collector = null;
 
     // collector factory
     public static function collector()
     {
-        if ( is_null( self::$_collector ) ) {
-            self::$_collector = new Collector( self::query() ); // Eloquent implemenation
-        }
+        $_collector = new Collector( self::query() ); // Eloquent implemenation
 
         // Assign/Implement delegates for the collector...
-        self::$_collector->_filterQueryDelegate = self::class.'::filterQuery';
-        self::$_collector->_searchQueryDelegate = self::class.'::searchQuery';
-        self::$_collector->_sortQueryDelegate = self::class.'::sortQuery';
+        $_collector->_filterQueryDelegate = self::class.'::filterQuery';
+        $_collector->_searchQueryDelegate = self::class.'::searchQuery';
+        $_collector->_sortQueryDelegate = self::class.'::sortQuery';
 
-        return self::$_collector;
+        return $_collector;
     }
 
     // %TODO: default implemenations (except for filter)
